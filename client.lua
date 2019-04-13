946de19a88513076aebf2ba5d7a02bdd79e23698
Lance Good <lgoodyt@gmail.com>
2019-04-13 01:11:05 -0400

    UwU

--- client.lua
+++ client.lua
@@ -1,121 +1,113 @@
--- Debug Gang
-
-function DrawTxt(text, x, y)
-	SetTextFont(0)
-	SetTextProportional(1)
-	SetTextScale(0.0, 0.4)
-	SetTextDropshadow(1, 0, 0, 0, 255)
-	SetTextEdge(1, 0, 0, 0, 255)
-	SetTextDropShadow()
-	SetTextOutline()
-	SetTextEntry("STRING")
-	AddTextComponentString(text)
-	DrawText(x, y)
-end
-
-Citizen.CreateThread(function()
-    while true do
-    	Citizen.Wait(0)
-		x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-		
-    	roundx = tonumber(string.format("%.2f", x))
-    	roundy = tonumber(string.format("%.2f", y))
-		roundz = tonumber(string.format("%.2f", z))
-		
-		DrawTxt("~r~X:~s~ "..roundx, 0.32, 0.00)
-		DrawTxt("~r~Y:~s~ "..roundy, 0.38, 0.00)
-		DrawTxt("~r~Z:~s~ "..roundz, 0.445, 0.00)
-
-		heading = GetEntityHeading(GetPlayerPed(-1))
-		roundh = tonumber(string.format("%.2f", heading))
-		DrawTxt("~r~H:~s~ "..roundh, 0.50, 0.00)
-
-        local rx,ry,rz = table.unpack(GetEntityRotation(PlayerPedId(), 1))
-		DrawTxt("~r~RX:~s~ "..tonumber(string.format("%.2f", rx)), 0.38, 0.03)
-		DrawTxt("~r~RY:~s~ "..tonumber(string.format("%.2f", ry)), 0.44, 0.03)
-		DrawTxt("~r~RZ:~s~ "..tonumber(string.format("%.2f", rz)), 0.495, 0.03)
-	
-		speed = GetEntitySpeed(PlayerPedId())
-		rounds = tonumber(string.format("%.2f", speed))
-		DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.92)
-
-		health = GetEntityHealth(PlayerPedId())
-		DrawTxt("~r~Player Health: ~s~"..health, 0.40, 0.95)
-
-		veheng = GetVehicleEngineHealth(GetVehiclePedIsUsing(PlayerPedId()))
-		vehbody = GetVehicleBodyHealth(GetVehiclePedIsUsing(PlayerPedId()))
-		if IsPedInAnyVehicle(PlayerPedId(), 1) then
-			vehenground = tonumber(string.format("%.2f", veheng))
-			vehbodround = tonumber(string.format("%.2f", vehbody))
-
-			DrawTxt("~r~Engine Health: ~s~"..vehenground, 0.015, 0.76)
-
-			DrawTxt("~r~Body Health: ~s~"..vehbodround, 0.015, 0.73)
-
-			DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(PlayerPedId())))), 0.015, 0.70)
-		end
-    end
-end)
-
 local table
 local lastvalid
 local ready = false
+local peacetime = 0
+local priority = false
+
+Citizen.CreateThread(function()
+	while true do
+		if (ready and table.name ~= nil) then
+			DrawTxt("~p~AOP~w~: ".. table.name .. "", 4, 1.60, 0.50, 255)
+		else
+			DrawTxt("~p~AOP~w~: Statewide", 4, 1.60, 0.50, 255)
+		end
+		DrawTxt("~p~Peacetime~w~: ".. (peacetime ~= 0 and "~g~"..peacetime.."~w~ minutes left" or "~r~Disabled") .. ".	~p~Priority~w~: ".. (priority and "~g~Enabled" or "~r~Disabled") .. ".", 4, 1.50, 0.50, 255)
+		Citizen.Wait(0)
+	end
+end)
+
+-- Debug Gang
+
+function DrawTxt(text, x, y, scale, opacity)
+    SetTextFont(4)
+    SetTextProportional(0)
+    SetTextScale(scale, scale)
+    SetTextColour(255, 255, 255, opacity)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextEdge(2, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(config.aopdisplay.x / x, config.aopdisplay.y / y)
+end
 
 function InitializeAOP(zone)
 	local playerPed = GetPlayerPed(-1)
-	if (zone == "default") then
-		table = config.zones[config.defaultzone]
-		lastvalid = table.location
-	else
-		table = zone
-		lastvalid = zone.location
+	table = zone
+	lastvalid = zone.location
+	if (IsPedInAnyVehicle(playerPed, true)) then
+		vehicle = GetVehiclePedIsIn(playerPed, false)
+		SetEntityAsMissionEntity(vehicle, false, false)
+		DeleteEntity(vehicle)
 	end
-	print("e")
 	SetEntityCoords(playerPed, table.location.x, table.location.y, table.location.z, 0, 0, 0, false)
 	ready = true
 end
 
 Citizen.CreateThread(function()
-	while ready do
-		local playerPed = GetPlayerPed(-1)
-		Citizen.Wait(0)
-		local plyCoords = GetEntityCoords(playerPed, false)
-		local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, table.location.x, table.location.y, table.location.z)
-		if dist <= table.radius / 2 then
-			if (dist <= (table.radius / 2) - 0.5) then
-				lastvalid = plyCoords
-			end
-		else
-			local vehicle
-			if (IsPedInAnyVehicle(playerPed, true)) then
-				vehicle = GetVehiclePedIsIn(playerPed, false)
-			end
-			local validnotfound = true
-			local index = 0
-			if (vehicle) then
-				SetEntityCoordsNoOffset(vehicle, lastvalid.x + index, lastvalid.y + index, lastvalid.z, 0, 0, 1)
-			else 
-				SetEntityCoords(playerPed, lastvalid.x + index, lastvalid.y + index, lastvalid.z, 0, 0, 0, false)
+	while true do
+		if (ready) then
+			local playerPed = GetPlayerPed(-1)
+			local plyCoords = GetEntityCoords(playerPed, false)
+			local x = (plyCoords.x - table.location.x)^2 
+			local y = (plyCoords.y - table.location.y)^2 
+			local z = (plyCoords.z - table.location.z)^2
+			local b = x + y + z
+			local location = vector3(table.location.x, table.location.y, table.location.z)
+			if #(plyCoords - location) < (table.radius) - (table.radius / 150) then
+				if #(plyCoords - location) < (table.radius) - (table.radius / 150) - 1.5 then
+					lastvalid = plyCoords
+				end
+			else
+				local vehicle
+				if (IsPedInAnyVehicle(playerPed, true)) then
+					vehicle = GetVehiclePedIsIn(playerPed, false)
+				end
+				local validnotfound = true
+				local index = 0
+				if (vehicle) then
+					SetEntityCoordsNoOffset(vehicle, lastvalid.x + index, lastvalid.y + index, lastvalid.z, 0, 0, 1)
+				else 
+					SetEntityCoords(playerPed, lastvalid.x + index, lastvalid.y + index, lastvalid.z, 0, 0, 0, false)
+				end
 			end
 		end
+		Citizen.Wait(0)
 	end
 end)
 
 Citizen.CreateThread(function()
-	InitializeAOP("default")
-	local playerPed = GetPlayerPed(-1)
-	while ready do
-		local plyCoords = GetEntityCoords(playerPed, false)
-		Citizen.Wait(0)
-		local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, table.location.x, table.location.y, table.location.z)
-		print(dist)
-		if (dist >= (table.radius / 2) - table.visabilitydistance) then
-			DrawMarker(1, table.location.x, table.location.y, table.location.z, 0, 0, 0, 0, 0, 0, table.radius + 0.0, table.radius + 0.0, table.height + 0.0, table.color.r, table.color.g, table.color.b, table.color.a, 0, 0, 0, 0)
+	while true do
+		if ready then
+			local playerPed = GetPlayerPed(-1)
+			local plyCoords = GetEntityCoords(playerPed, false)
+			local location = vector3(table.location.x, table.location.y, table.location.z)
+			if (#(plyCoords - location) > (table.radius) - (table.radius / 150) - table.visabilitydistance) then
+				DrawMarker(28, table.location.x, table.location.y, table.location.z, 0, 0, 0, 0, 0, 0, table.radius + 0.0, table.radius + 0.0, table.radius + 0.0, table.color.r, table.color.g, table.color.b, table.color.a, 0, 0, 0, 0)
+			end
 		end
+		Citizen.Wait(0)
 	end
+end)
+
+Citizen.CreateThread(function()
+	TriggerServerEvent("aop:UpdateClientAOP")
+	return
 end)
 
 RegisterNetEvent("aop:InitializeAOP")
 AddEventHandler("aop:InitializeAOP", function(zone)
 	InitializeAOP(zone)
+end)
+
+RegisterNetEvent("aop:SetStatus")
+AddEventHandler("aop:SetStatus", function(status)
+	ready = status
+end)
+
+RegisterNetEvent("aop:UpdateInformation")
+AddEventHandler("aop:UpdateInformation", function(info)
+	peacetime 	= 	info[1]
+	priority 	=  	info[2]
 end)
--- config.lua
+++ config.lua
@@ -1,14 +1,25 @@
 config = {
     zones = {
         ["Sandy Shores"] = {
-            cmd         = "ss", -- You use this as the argument for the AOP change. (usage: /aop [cmd]) CANNOT BE "list"
-            location    = {x = 1809.0, y = 3676.0, z = 34.0},
-            color       = {r = 255, g = 0, b = 0, a = 100}, -- Red (0-255), Blue (0-255), Green (0-255), Transparancy (0-255)
-            visabilitydistance = 100, -- You will see the zone's boundaries when you are this far away from the boundaries.
-            height      = 3000, -- NOTE: YOUR HEIGHT JUST EFFECTS YOUR MARKER'S DISPLAYED HEIGHT. RADIUS AUTOMATICALLY USES ITS NUMBER AS THE ACTUAL DETECTION HEIGHT. (MAKE IT 1000+)
-            radius      = 3000 -- The zone's radius.
+            cmd                 = "ss", -- You use this as the argument for the AOP change. (usage: /aop [cmd]) CMD CANNOT BE "list", "start", "stop", "peacetime", or "priority".
+            location            = {x = 1410.51, y = 3122.768, z = 40.55616},
+            color               = {r = 255, g = 0, b = 0, a = 100}, -- Red (0-255), Blue (0-255), Green (0-255), Transparancy (0-255)
+            visabilitydistance  = 1500, -- You will see the zone's boundaries when you are this far away from the boundaries.
+            radius              = 1500 -- The zone's radius.
+        },
+        ["Blandy Shores"] = {
+            cmd                 = "bs", -- You use this as the argument for the AOP change. (usage: /aop [cmd]) CMD CANNOT BE "list", "start", "stop", "peacetime", or "priority".
+            location            = {x = 1709.0, y = 3776.0, z = 34.0},
+            color               = {r = 255, g = 0, b = 0, a = 100}, -- Red (0-255), Blue (0-255), Green (0-255), Transparancy (0-255)
+            visabilitydistance  = 100, -- You will see the zone's boundaries when you are this far away from the boundaries.
+            radius              = 100 -- The zone's radius.
         },
     },
-    defaultzone = "Sandy Shores",
+    admins = {
+        'steam:11000010ce19afe', -- Remove this unless you want to honor my existance
+        'steam:110000114192c42', -- Remove this unless you want to honor my existance
+    },
+    aopdisplay = {x = 0.660, y = 1.370},
+    defaultaop = "Sandy Shores", -- The zone name that is loaded on the server's launch.
     killswitchcode = "yunggood" -- DOING "/aop [killswitchcode]" WILL STOP THE ADDON FROM ENFORCING BOUNDARIES AND DISPLAYING MARKERS. YOU CAN TYPE THE CODE AGAIN TO RE-ENABLE THE RESOURCE, BUT YOU WILL BE SENT TO THE DEFAULT AOP.
 }
